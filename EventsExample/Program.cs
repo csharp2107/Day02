2eb34905c6a70e42b7a04b0a3b97015b894d1ae6
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-22 13:24:48 +0200

    update

--- EventsExample/Program.cs
+++ EventsExample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EventsExample
@@ -34,9 +35,15 @@ namespace EventsExample
         // declare event based on delegate CarsComputerHandler
         public event CarsComputerHandler CarsComputerEventLog;
 
+        Random rnd = new Random();
         public void ProcessLog()
         {
-
+            for (int i = 0; i < 5; i++)
+            {
+                String s = $"Temp: {rnd.Next(70, 110)}, pressure: {rnd.Next(5, 8)}";
+                OnCardComputerEventLog(s);
+                Thread.Sleep(1000);
+            }
         }
 
         public void OnCardComputerEventLog(string info)
@@ -50,6 +57,29 @@ namespace EventsExample
     {
         static void Main(string[] args)
         {
+            DelegateOnBoardComputerEvent ce = new DelegateOnBoardComputerEvent();
+            //ce.CarsComputerEventLog +=
+            //    new DelegateOnBoardComputerEvent.CarsComputerHandler(DisplayInformation);
+            ce.CarsComputerEventLog += Ce_CarsComputerEventLog;
+
+            Thread thread = new Thread(() =>
+           {
+               ce.ProcessLog();
+           });
+            thread.Start();
+            
+
+            Console.ReadKey();
+        }
+
+        private static void Ce_CarsComputerEventLog(string info)
+        {
+            Console.WriteLine(info);
+        }
+
+        static void DisplayInformation(string info)
+        {
+            Console.WriteLine(info);
         }
     }
 }
