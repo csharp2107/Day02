c1a8627f9e439ad88c942de9bfd94f5cae8164ca
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-22 15:56:14 +0200

    update

--- FetchImages/Form1.cs
+++ FetchImages/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,9 +20,41 @@ namespace FetchImages
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        String baseURL = "http://51.91.120.89/TABLICE/";
+        private async void button1_Click(object sender, EventArgs e)
         {
-            Thread.Sleep(5000);
+            List<Task> tasks = new List<Task>();
+
+            WebClient wb = new WebClient();
+            string content = wb.DownloadString(baseURL);
+            string[] lines = content.Split('\n');
+            foreach (var line in lines)
+            {
+
+                String imageFile = line.Trim();
+                if (String.IsNullOrEmpty(imageFile))
+                    continue;
+
+                Task task = Task.Run( ()=> {
+
+                    String url = $"{baseURL}{imageFile}";
+
+                    WebClient client = new WebClient();
+                    byte[] data = client.DownloadData(url);
+                    using (FileStream fs = new FileStream("c:/tmp/"+imageFile, FileMode.Create))
+                    {
+                        fs.Write(data, 0, data.Length);
+                        fs.Flush();
+                    }
+
+                } );
+                tasks.Add(task);
+                //break;
+
+            }
+
+            await Task.WhenAll(tasks);
+
         }
     }
 }
